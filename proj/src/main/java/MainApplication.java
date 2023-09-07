dbb7816c6aeccbab8b7a260e23b8ad14659309dc
Yuliya Ulupova <git.user>
2023-09-07 23:45:12 +0300

    Изменена основная логика Добавлен класс коробки

--- proj/src/main/java/MainApplication.java
+++ proj/src/main/java/MainApplication.java
@@ -1,7 +1,12 @@
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111111");
+        System.out.println("110001");
         System.out.println(22222);
+    }
+
+    public static void doSomething() {
+        System.out.println(0000);
+
     }
 }
