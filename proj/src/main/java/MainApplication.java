082ee998225b6e17fe5467fa89a0eb7ab5a6b654
Yuliya Ulupova <git.user>
2023-09-07 23:14:01 +0300

    Доработка логики проекта версия1

--- proj/src/main/java/MainApplication.java
+++ proj/src/main/java/MainApplication.java
@@ -2,5 +2,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("111111");
+        System.out.println(22222);
     }
 }
