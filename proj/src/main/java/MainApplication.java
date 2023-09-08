62b026a482cf46ed568cd294e6e7e9cca654b567
Yuliya Ulupova <git.user>
2023-09-08 18:29:14 +0300

    Изменили логику main и doSomething()

--- proj/src/main/java/MainApplication.java
+++ proj/src/main/java/MainApplication.java
@@ -1,14 +1,15 @@
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println(---);
         System.out.println("Hello World!");
         System.out.println("110001");
         System.out.println(22222);
     }
 
     public static void doSomething() {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
 
     }
 }
