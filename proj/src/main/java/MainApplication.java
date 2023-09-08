2fbffbc24ed871321b1aee001f612e07c54bb60f
Yuliya Ulupova <git.user>
2023-09-08 18:19:37 +0300

    Улучшили логику main и doSomething()

--- proj/src/main/java/MainApplication.java
+++ proj/src/main/java/MainApplication.java
@@ -3,12 +3,13 @@ public class MainApplication {
         System.out.println("Hello World!");
         System.out.println("110001");
         System.out.println(22222);
+        System.out.println(---);
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
 
     }
 }
